68d271c64b62cb79aa4a4d4d3f55585d1bbd49aa
Oswaldo-kvallo <youngfrenk3@gmail.com>
2024-08-05 20:35:47 -0600

    Hice que en la canasta al dar en agregar mas productos pues no se ponga mas del limite que se puso en Productos y pues si se pone que un producto tiene 0 de cantidad diga que no se puede agregar a la canasta

--- src/components/NuevaVenta.vue
+++ src/components/NuevaVenta.vue
@@ -1,68 +1,66 @@
 <template>
   <div class="container-fluid text-bg-light p-3">
     <div class="row">
-      <div class="col-lg-12">
-        <div class="row">
-          <!-- Navbar para búsqueda -->
-          <div class="col-lg-8 col-md-12 mb-4">
-            <div class="row nav">
-              <div class="col-lg-12">
-                <nav class="navbar navbar-expand-lg bg-body-tertiary nav2">
-                  <div class="container-fluid">
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                      <ul class="navbar-nav me-auto mb-2 col-lg-12">
-                        <form class="d-flex" @submit.prevent="searchProduct">
-                          <input class="form-control me-2" v-model="searchQuery" type="search" placeholder="Buscar Producto" aria-label="Buscar">
-                          <button class="btn btn-outline-success" type="submit" style="color: blue; border-color: blue;">Buscar</button>
-                        </form>
-                      </ul>
-                    </div>
-                  </div>
-                </nav>
-              </div>
-              <!-- Tarjetas de productos -->
-              <div class="col-lg-12 mt-3">
-                <div class="card card1" v-for="product in filteredProducts" :key="product.IDProducto" style="width: 100%;">
-                  <img :src="`data:image/jpeg;base64,${product.Datos}`" class="card-img-top" alt="Product Image" height="200px" width="100px" style="border-radius: 10px;">
-                  <div class="card-body">
-                    <h5 class="card-title">{{ product.Nombre }}</h5>
-                    <p class="card-text">{{ product.Descripcion }}</p>
-                    <h5>{{ product.PrecioVenta }}</h5>
-                    <button class="btn btn-primary" type="button" @click="addProductToCart(product)">Agregar Producto</button>
-                  </div>
-                </div>
+      <!-- Sección de búsqueda y productos -->
+      <div class="col-lg-8 col-md-12 mb-4">
+        <!-- Navbar para búsqueda -->
+        <nav class="navbar navbar-expand-lg bg-body-tertiary nav2">
+          <div class="container-fluid">
+            <form class="d-flex" @submit.prevent="searchProduct">
+              <input class="form-control me-2" v-model="searchQuery" type="search" placeholder="Buscar Producto"
+                aria-label="Buscar">
+              <button class="btn btn-outline-success" type="submit"
+                style="color: blue; border-color: blue;">Buscar</button>
+            </form>
+          </div>
+        </nav>
+
+        <!-- Tarjetas de productos -->
+        <div class="row mt-3">
+          <div class="col-lg-12">
+            <div class="card card1" v-for="product in filteredProducts" :key="product.IDProducto" style="width: 100%;">
+              <img :src="'data:image/jpeg;base64,' + product.Datos" class="card-img-top" alt="Product Image"
+                style="border-radius: 10px;">
+              <div class="card-body">
+                <h5 class="card-title">{{ product.Nombre }}</h5>
+                <p class="card-text">{{ product.Descripcion }}</p>
+                <h5>{{ product.PrecioVenta }}</h5>
+                <button class="btn btn-primary" type="button" @click="addProductToCart(product)">Agregar
+                  Producto</button>
               </div>
             </div>
           </div>
+        </div>
+      </div>
 
-          <!-- Sección de Canasta -->
-          <div class="col-lg-4 col-md-12">
-            <div class="card canasta-card">
-              <div class="card-header d-flex justify-content-between align-items-center">
-                <h5 class="mb-0">Canasta</h5>
-                <button class="btn btn-secondary btn-sm" @click="clearCart">Vaciar</button>
-              </div>
-              <div class="card-body">
-                <div v-for="item in cart" :key="item.IDProducto" class="mb-3">
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div>
-                      <h6>{{ item.Nombre }}</h6>
-                      <small>{{ item.Cantidad }} x {{ item.PrecioVenta }}</small>
-                      <div>
-                        <button class="btn btn-sm btn-secondary" @click="updateQuantity(item, -1)" :disabled="item.Cantidad <= 1">-</button>
-                        <button class="btn btn-sm btn-secondary" @click="updateQuantity(item, 1)">+</button>
-                      </div>
-                    </div>
-                    <button class="btn btn-danger btn-sm" @click="removeFromCart(item)">Eliminar</button>
+      <!-- Sección de Canasta -->
+      <div class="col-lg-4 col-md-12">
+        <div class="card canasta-card">
+          <div class="card-header d-flex justify-content-between align-items-center">
+            <h5 class="mb-0">Canasta</h5>
+            <button class="btn btn-secondary btn-sm" @click="clearCart">Vaciar</button>
+          </div>
+          <div class="card-body">
+            <div v-for="item in cart" :key="item.IDProducto" class="mb-3">
+              <div class="d-flex justify-content-between align-items-center">
+                <div>
+                  <h6>{{ item.Nombre }}</h6>
+                  <small>{{ item.Cantidad }} x {{ item.PrecioVenta }}</small>
+                  <div class="mt-2">
+                    <button class="btn btn-sm btn-secondary" @click="updateQuantity(item, -1)"
+                      :disabled="item.Cantidad <= 1">-</button>
+                    <button class="btn btn-sm btn-secondary" @click="updateQuantity(item, 1)">+</button>
                   </div>
                 </div>
-                <div class="form-group">
-                  <label for="total">Total</label>
-                  <input type="text" class="form-control" id="total" :value="totalPrice" readonly>
-                </div>
-                <button class="btn btn-primary mt-3" data-bs-toggle="offcanvas" href="#offcanvasRight" role="button" aria-controls="offcanvasRight" @click="prepareConfirmation">Confirmar Producto</button>
+                <button class="btn btn-danger btn-sm" @click="removeFromCart(item)">Eliminar</button>
               </div>
             </div>
+            <div class="form-group mt-3">
+              <label for="total">Total</label>
+              <input type="text" class="form-control" id="total" :value="totalPrice" readonly>
+            </div>
+            <button class="btn btn-primary mt-3" data-bs-toggle="offcanvas" href="#offcanvasRight" role="button"
+              aria-controls="offcanvasRight" @click="prepareConfirmation">Confirmar Producto</button>
           </div>
         </div>
       </div>
@@ -71,59 +69,30 @@
     <!-- Offcanvas -->
     <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
       <div class="offcanvas-header">
-        <h5 id="offcanvasRightLabel">Confirmacion de la venta</h5>
+        <h5 id="offcanvasRightLabel">Confirmación de la venta</h5>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
       <div class="offcanvas-body">
-        <div class="movements">
-          <div class="tab-buttons">
-            <button @click="showSection('transacciones')" :class="{active: currentSection === 'transacciones'}">Pagada</button>
-            <button @click="showSection('cierresCaja')" :class="{active: currentSection === 'cierresCaja'}">A credito</button>
-          </div>
-          <div class="transaction-section" v-if="currentSection === 'transacciones'">
-            <div>
-              <label for="text">Dale nombre a esta venta</label>
-              <input type="text" />
-              <label for="text">Fecha de la venta</label>
-              <input type="date" />
-            </div>
-            <div class="transaction-table">
-              <div class="opciones">
-                <button type="button" class="btn btn-outline-secondary" :class="{ active: selectedPaymentMethod === 'efectivo' }" @click="selectPaymentMethod('efectivo')">Efectivo<i class="bi bi-cash"></i></button>
-                <button type="button" class="btn btn-outline-secondary" :class="{ active: selectedPaymentMethod === 'tarjeta' }" @click="selectPaymentMethod('tarjeta')">Tarjeta<i class="bi bi-credit-card"></i></button>
-                <button type="button" class="btn btn-outline-secondary" :class="{ active: selectedPaymentMethod === 'transferencia' }" @click="selectPaymentMethod('transferencia')">Transferencia bancaria<i class="bi bi-bank2"></i></button>
-                <button type="button" class="btn btn-outline-secondary" :class="{ active: selectedPaymentMethod === 'otro' }" @click="selectPaymentMethod('otro')">Otro<i class="bi bi-columns-gap"></i></button>
-              </div>
-              <div>
-                <label for="buscar">Agrega un cliente a la venta (opcional)</label>
-                <input type="text">
-              </div>
-              <div>
-                <button type="button" class="btn btn-primary" @click="confirmProduct">Confirmar venta</button>
-              </div>
-            </div>
-          </div>
-          <div class="cierres-caja-section" v-if="currentSection === 'cierresCaja'">
-            <div>
-              <div>
-                <label for="text">Dale nombre a esta venta</label>
-              <input type="text" />
-                <label for="text">Fecha de la venta</label>
-                <input type="date" />
-              </div>
-              <div>
-                <label for="buscar">Agrega un cliente a la venta (opcional)</label>
-                <input type="text">
-              </div>
-              <div>
-                <button type="submit" class="btn btn-primary">Confirmar venta</button>
-              </div>
-            </div>
-          </div>
-          <!-- Mostrar el total en el offcanvas -->
-          <div class="total-section">
-            <h5>Total de la venta: {{ totalPrice }}</h5>
-          </div>
+
+
+        <div class="d-flex mb-3">
+          <button @click="showSection('pagada')" :class="{ active: currentSection === 'pagada' }"
+            class="btn btn-outline-secondary flex-fill me-2">Efectivo<i class="bi bi-cash"></i></button>
+          <button @click="showSection('credito')" :class="{ active: currentSection === 'credito' }"
+            class="btn btn-outline-secondary flex-fill">Tarjeta<i class="bi bi-credit-card"></i></button>
+        </div>
+        <div v-if="currentSection === 'credito'" class="mb-3">
+          <label for="clienteNombre">Nombre del Cliente</label>
+          <input type="text" id="clienteNombre" v-model="clientName" class="form-control" />
+          <label for="clienteTelefono">Teléfono del Cliente</label>
+          <input type="text" id="clienteTelefono" v-model="clientPhone" class="form-control" />
+
+        </div>
+        <div class="mb-3">
+          <button type="button" class="btn btn-primary" @click="confirmProduct">Confirmar venta</button>
+        </div>
+        <div class="total-section mt-3">
+          <h5>Total de la venta: {{ totalPrice }}</h5>
         </div>
       </div>
     </div>
@@ -137,11 +106,15 @@ export default {
   name: 'NuevaVenta',
   data() {
     return {
-      products: [], // Array para almacenar los productos
-      searchQuery: '', // Variable para almacenar la consulta de búsqueda
-      cart: [], // Array para almacenar los productos en el carrito
-      currentSection: 'transacciones', // Define la sección inicial
-      selectedPaymentMethod: '', // Variable para almacenar el método de pago seleccionado
+      products: [],
+      searchQuery: '',
+      cart: [],
+      currentSection: 'pagada',
+      selectedPaymentMethod: '',
+      IDNeg: 1,
+      clientName: '',
+      clientPhone: '',
+      clientEmail: ''
     };
   },
   computed: {
@@ -160,7 +133,7 @@ export default {
   methods: {
     async fetchProducts() {
       try {
-        const response = await axios.get('http://localhost:3000/Producto'); // URL del endpoint
+        const response = await axios.get('http://localhost:3000/Producto');
         this.products = response.data.results;
       } catch (error) {
         console.error('Error al obtener productos:', error);
@@ -170,9 +143,18 @@ export default {
       // La búsqueda ya se maneja a través de la propiedad computada
     },
     addProductToCart(product) {
+      if (product.CantidadExistencia <= 0) {
+        alert('No se puede agregar el producto. La cantidad es 0.');
+        return;
+      }
+      
       const cartItem = this.cart.find(item => item.IDProducto === product.IDProducto);
       if (cartItem) {
-        cartItem.Cantidad++;
+        if (cartItem.Cantidad < product.CantidadExistencia) {
+          cartItem.Cantidad++;
+        } else {
+          alert('No se puede agregar más cantidad del producto. Cantidad máxima alcanzada.');
+        }
       } else {
         this.cart.push({ ...product, Cantidad: 1 });
       }
@@ -184,18 +166,29 @@ export default {
       this.cart = [];
     },
     prepareConfirmation() {
-      // Esta función es para cualquier preparación necesaria antes de mostrar el offcanvas
+      // Preparar cualquier acción necesaria antes de mostrar el offcanvas
     },
     async confirmProduct() {
       try {
-        // Aquí se llama al endpoint para confirmar la venta
-        const response = await axios.post('http://localhost:3000/confirmarVenta', {
-          cart: this.cart,
+        const saleData = {
           total: this.totalPrice,
-          paymentMethod: this.selectedPaymentMethod
-        });
+          cart: this.cart,
+          clientName: this.clientName,
+          clientPhone: this.clientPhone,
+          selectedPaymentMethod: this.selectedPaymentMethod,
+          IDNeg: this.IDNeg
+        };
+        const response = await axios.post('http://localhost:3000/confirmventa', saleData);
         console.log('Venta confirmada:', response.data);
-        this.clearCart(); // Vaciar la canasta después de confirmar la venta
+
+        // Obtener los datos del último registro
+        const dataResponse = await axios.get('http://localhost:3000/ventaexit');
+        const datosventaex = dataResponse.data;
+        alert(`Venta Exitosa:
+            ID Venta: ${datosventaex.IDVenta}
+            Total: ${datosventaex.CostoTotal}
+            ID Negocio: ${datosventaex.IDNegocio}`);
+        this.clearCart();
       } catch (error) {
         console.error('Error al confirmar venta:', error);
       }
@@ -206,21 +199,28 @@ export default {
         cartItem.Cantidad += amount;
         if (cartItem.Cantidad <= 0) {
           this.removeFromCart(cartItem);
+        } else if (cartItem.Cantidad > product.CantidadExistencia) {
+          alert('No se puede agregar más cantidad del producto. Cantidad máxima alcanzada.');
+          cartItem.Cantidad = product.CantidadExistencia;
         }
       }
     },
     showSection(section) {
       this.currentSection = section;
-    },
-    selectPaymentMethod(method) {
-      this.selectedPaymentMethod = method;
+      if (section === 'credito') {
+        this.selectedPaymentMethod = 'Credito';
+      } else {
+        this.selectedPaymentMethod = 'Efectivo';
+      }
     }
   },
   mounted() {
-    this.fetchProducts(); // Obtener los productos cuando se monta el componente
+    this.fetchProducts();
   }
 };
 </script>
+
+
 
 <style scoped>
 .card1 {
@@ -239,10 +239,9 @@ export default {
   width: 100%;
 }
 
-.offcanvas-title {
+.offcanvas-header {
   font-size: 20px;
   font-weight: bold;
-  margin-bottom: 20px;
 }
 
 .offcanvas-body label {
@@ -252,23 +251,13 @@ export default {
 }
 
 .offcanvas-body input[type="date"],
-.offcanvas-body .btn-outline-secondary,
-.offcanvas-body input[type="text"] {
+.offcanvas-body input[type="text"],
+.offcanvas-body input[type="email"] {
   width: 100%;
   padding: 10px;
   margin-bottom: 10px;
   border-radius: 5px;
   border: 1px solid #ccc;
-}
-
-.offcanvas-body .opciones {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 10px;
-}
-
-.offcanvas-body .opciones button {
-  flex: 1 1 calc(50% - 10px);
 }
 
 .offcanvas-body .btn-primary {
@@ -278,23 +267,15 @@ export default {
   border-radius: 5px;
 }
 
-.tab-buttons button {
-  background-color: transparent;
-  border: none;
-  font-size: 16px;
-  margin-right: 20px;
-  cursor: pointer;
-}
-
 .tab-buttons {
   display: flex;
-  justify-content: space-between;
   margin-bottom: 20px;
 }
 
 .tab-buttons button {
   flex: 1;
   margin-right: 10px;
+  border-radius: 5px;
 }
 
 .tab-buttons button:last-child {
@@ -302,11 +283,6 @@ export default {
 }
 
 .tab-buttons button.active {
-  font-weight: bold;
-  border-bottom: 2px solid #000;
-}
-
-button.active {
   background-color: #007bff;
   color: white;
   border-color: #007bff;
